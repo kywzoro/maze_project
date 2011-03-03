5eea69eb6df2205cb7b066d646818a684c4f1511
kywzoro <kywzoro@nate.com>
2011-03-04 01:23:41 +0900

    part4 test

---
+++ linux_dd/chap2/part4/.tmp_versions/hello-4.mod
@@ -0,0 +1,2 @@
+/home/beagle/maze_project/linux_dd/chap2/part4/hello-4.ko
+/home/beagle/maze_project/linux_dd/chap2/part4/hello-4.o
---
+++ linux_dd/chap2/part4/hello-4.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#define DRIVER_AUTHOR	"Peter Jay Salzman <p@dirac.org>"
+#define DRIVER_DESC	"A sample driver"
+
+
+static int __init init_hello_4(void)
+{
+
+    printk(KERN_INFO "Hello, World 4\n");
+    return 0;
+
+}
+
+static void __exit cleanup_hello_4(void)
+{
+    printk(KERN_INFO "GoodBye World 4.\n");
+}
+
+
+module_init(init_hello_4);
+module_exit(cleanup_hello_4);
+
+MODULE_LICENSE("GPL");
+
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+
+MODULE_SUPPORTED_DEVICE("testdevice");
